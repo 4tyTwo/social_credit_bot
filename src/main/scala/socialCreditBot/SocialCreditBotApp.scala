ab9bdc7b44dfefc595de8c1a45fb1fde8f589dbe
Igor Toporkov <iigortoporkov@gmail.com>
2022-04-19 01:42:49 +0200

    Implement simple server to comply with Heroku requirements

--- src/main/scala/socialCreditBot/SocialCreditBotApp.scala
+++ src/main/scala/socialCreditBot/SocialCreditBotApp.scala
@@ -1,8 +1,10 @@
 package socialCreditBot
 
+import com.sun.net.httpserver.{HttpExchange, HttpHandler, HttpServer}
 import socialCreditBot.bot.SocialCreditBot
 import socialCreditBot.repository.UserRatingRepository
 
+import java.net.InetSocketAddress
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
@@ -11,6 +13,19 @@ object SocialCreditBotApp extends App {
   val db = new UserRatingRepository()
   val bot = new SocialCreditBot(sys.env("BOT_TOKEN"), db)
   val eol = bot.run()
+  println("Port: " + sys.env("PORT"))
+  val server = HttpServer.create(new InetSocketAddress(sys.env("PORT").toInt), 0)
+  server.createContext("/", new HttpHandler() {
+    override def handle(exchange: HttpExchange): Unit = {
+      val response = "Ack!"
+      exchange.sendResponseHeaders(200, response.length())
+      val os = exchange.getResponseBody
+      os.write(response.getBytes)
+      os.close()
+    }
+  })
+  server.setExecutor(null)
+  server.start()
   // Wait for the bot end-of-life
   Await.result(eol, Duration.Inf)
 }
