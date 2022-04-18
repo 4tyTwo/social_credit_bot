939521bfa69022d7e8089c593e7b051a4cb7254d
Igor Toporkov <iigortoporkov@gmail.com>
2022-04-19 01:31:11 +0200

    Remove no longer needed shutdowns

--- src/main/scala/socialCreditBot/SocialCreditBotApp.scala
+++ src/main/scala/socialCreditBot/SocialCreditBotApp.scala
@@ -11,10 +11,6 @@ object SocialCreditBotApp extends App {
   val db = new UserRatingRepository()
   val bot = new SocialCreditBot(sys.env("BOT_TOKEN"), db)
   val eol = bot.run()
-  println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
-  scala.io.StdIn.readLine()
-  bot.shutdown()
-  db.db.close()
   // Wait for the bot end-of-life
   Await.result(eol, Duration.Inf)
 }
